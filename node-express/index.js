8e2e5e7706b32a99947a893c2d628209147b7953
StuartCreed <stuart.a.creed@gmail.com>
2020-01-18 18:15:20 +0000

    Express Router

--- node-express/index.js
+++ node-express/index.js
@@ -5,9 +5,20 @@ const hostname = 'localhost';
 const port = 3000;
 const app = express();
 const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use('/dishes', dishRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
 
 app.all('/dishes', (req,res,next) => {
   res.statusCode = 200;
